c226084be1d28d9d09085ed25121b8cafd774e3d
alex-skxy <aleksandra.kina@sluz.ch>
2022-02-02 10:39:44 +0100

    u1-1: return hello

--- m151/u1-1/server.mjs
+++ m151/u1-1/server.mjs
@@ -1,6 +1,17 @@
 import {createServer} from 'http';
 
-const server = createServer();
+const server = createServer((req, res) => {
+    if (req.url === '/') {
+        // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+
+        // set response content
+        res.write('hello :)');
+        res.end();
+    }
+});
+
+
 server.listen(8080, () => {
     console.log(`listening on ${server.address().port}`);
 })
